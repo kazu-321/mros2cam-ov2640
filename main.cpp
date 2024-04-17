4687b37a70ff76c7f9aedf9fa591ebdb620f35c9
kazu <kzs321kzs@gmail.com>
2024-04-17 15:08:29 +0900

    first

--- main.cpp
+++ main.cpp
@@ -1,64 +1,96 @@
-/* mros2 example
- * Copyright (c) 2022 mROS-base
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// include here
+#include "ThisThread.h"
+#include "logging.h"
+#include "mbed.h"
 #include "mros2.h"
 #include "mros2-platform.h"
-#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include <cstdint>
+#include <cstdio>
+#include <math.h>
+#include <vector>
+#include "arducam.h"
+// using and define here
+using namespace std;
 
-void userCallback(std_msgs::msg::String *msg)
-{
-  MROS2_INFO("subscribed msg: '%s'", msg->data.c_str());
+ArduCAM cam(PC_12,PC_11,PC_10,PD_2,PB_9,PB_8);          // カメラ
+void cam_fn();
+
+mros2::Publisher pub_cam;
+
+
+// プログラムhere
+int main(){
+    MROS2_INFO("start");
+    if (mros2_platform::network_connect()) {            // connect to the network
+        MROS2_ERROR("ネットつながってないにょ...");
+        return -1;
+    } else {
+        MROS2_INFO("ネットつながた！---");
+    }
+
+
+    MROS2_INFO("カメラ起動");
+    cam.InitCAM();
+    MROS2_INFO("カメラﾖｼ！");
+
+    MROS2_INFO("mini robot起動します");
+
+    mros2::init(0, NULL);       // 初期化
+    MROS2_DEBUG("mROS 2初期化完了");
+
+    mros2::Node node = mros2::Node::create_node("mros2_node");  // node生成
+    mros2::Publisher pub_cam = node.create_publisher<sensor_msgs::msg::Image>("image",10);
+
+    osDelay(100);
+    MROS2_INFO("送受信準備完了！");
+    cam_fn();
+    mros2::spin();
+    return 0;
 }
 
-int main()
-{
-  /* connect to the network */
-  if (mros2_platform::network_connect())
-  {
-    MROS2_ERROR("failed to connect and setup network! aborting,,,");
-    return -1;
-  }
-  else
-  {
-    MROS2_INFO("successfully connect and setup network\r\n---");
-  }
+void cam_fn(){
+    ThisThread::sleep_for(2s);
+    while(true){
+        cam.write_reg(0x00,0x56);
+        if(cam.read_reg(0x00)==0x56){
+            printf("ok!!\n");
+            break;
+        }else{
+            printf("spi error...\n");
+        }
+        ThisThread::sleep_for(100ms);
+    }
+    while(true){
+        cam.flush_fifo();
+        cam.clear_fifo_flag();
+        cam.start_capture();
+        while(!cam.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK));
+        uint32_t imageLength = cam.read_fifo_length();
+        printf("data %d\n",imageLength);
+        cam.CS_LOW();
+        cam.set_fifo_burst();
+        auto message=sensor_msgs::msg::Image();
+        message.frame_id="camera_flame";
+        message.nanosec=0;
+        message.sec=time(NULL);
+        message.height=240;
+        message.width=320;
+        message.encoding="rgb";
+        message.is_bigendian=0;
+        message.step=320*3;
+        message.data.resize(imageLength);
+        // vector<uint8_t> imageData(imageLength);
+        for(int i=0;i<imageLength;i++){
+            uint8_t byte=cam.read_fifo();
+            // printf("%d,",byte);
+            // imageData[i]=byte;
+            message.data[i]=byte;
+        }
 
-  MROS2_INFO("%s start!", MROS2_PLATFORM_NAME);
-  MROS2_INFO("app name: echoback_string");
-
-  mros2::init(0, NULL);
-  MROS2_DEBUG("mROS 2 initialization is completed");
-
-  mros2::Node node = mros2::Node::create_node("mros2_node");
-  mros2::Publisher pub = node.create_publisher<std_msgs::msg::String>("to_linux", 10);
-  mros2::Subscriber sub = node.create_subscription<std_msgs::msg::String>("to_stm", 10, userCallback);
-
-  osDelay(100);
-  MROS2_INFO("ready to pub/sub message\r\n---");
-
-  auto count = 0;
-  while (1)
-  {
-    auto msg = std_msgs::msg::String();
-    msg.data = "Hello from " + std::string(MROS2_PLATFORM_NAME) + " onto " + quote(TARGET_NAME) + ": " + std::to_string(count++);
-    MROS2_INFO("publishing msg: '%s'", msg.data.c_str());
-    pub.publish(msg);
-    osDelay(1000);
-  }
-
-  mros2::spin();
-  return 0;
+        cam.CS_HIGH();
+        cam.clear_fifo_flag();
+        // message.data=imageData;
+        pub_cam.publish(message);
+    }
 }
